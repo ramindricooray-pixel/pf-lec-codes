f3715837a3f9e5c5b23b5a1a6f6eed4d90877ed0
ramindricooray-pixel <ramindricooray@gmail.com>
2026-04-28 14:47:16 +0530

    Update pf lec codes.py

--- pf lec codes.py
+++ pf lec codes.py
@@ -8,4 +8,23 @@ while True:
         break
     else:
         print("Incorrect PIN. Please try again.")
-        
+
+
+
+
+
+total_bill = 0
+
+for i in range(1, 6):
+    price = float(input(f"Enter price for item {i}: "))
+    total_bill += price
+
+discount = 0
+if total_bill > 5000:
+    discount = total_bill * 0.20
+
+final_amount = total_bill - discount
+
+print(f"Total Amount: Rs. {total_bill}")
+print(f"Discount Applied: Rs. {discount}")
+print(f"Final Payable Amount: Rs. {final_amount}")
